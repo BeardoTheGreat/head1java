a5d7f18c1e4d0bc1f8f7706828d8cbba98e9d531
BeardoTheGreat <chad.snyder1338@gmail.com>
2017-02-08 23:50:50 -0800

    First commit

---
+++ chapter1/PoolPuzzle.java
@@ -0,0 +1,36 @@
+class PoolPuzzle{
+    public static void main (String [] args ) {
+        int x = 0;
+                //insert math function in paranthesis
+        while ( x < 4 ){
+            //print something
+            System.out.print("a");
+            if ( x < 1) {
+                //print something
+                System.out.print(" ");
+            }
+            //print something
+            System.out.print("n");
+            if ( x > 1 ) {
+                //print something
+                System.out.print(" oyster!");
+                //math function?
+                x = x + 2;
+            }
+            if ( x == 1) {
+                //print something
+                System.out.print("noys");
+            }
+            if ( x < 1 ) {
+                //print something
+                System.out.print("oise");
+            }
+            System.out.println(" ");
+            //math function
+            x = x + 1;
+
+        }
+
+            System.out.print("\n");
+    }
+}
